44ff3b32df100cd9729dfe15f1fbf8e9f53bed06
Youhoseong <h0song@naver.com>
2021-04-18 00:15:56 +0900

    docs: test input add

---
+++ codein.java
@@ -0,0 +1,30 @@
+public class Codein {
+    public static void main(String args[]) {
+        char a = '1';
+        char b = 'y';
+        int x = a + b;
+        int[] array = {56, 14};
+        boolean bo = false;
+        String str = "this is string";
+
+        int _testvar123 = 0;
+
+        if(x >= 12) {
+            b = '2';
+        }
+        else if(x != 15){
+            b = '3';
+        }
+        else {
+            b = a / 3;
+        }
+
+        while(true) {
+
+        }
+    
+        return 0;
+    }
+
+
+}
